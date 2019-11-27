901c131245838d3961039df7da1b28310af181fa
xbindt <xbindt@hotmail.com>
2019-11-27 11:00:00 +0100

    Update _document.js

--- www/pages/_document.js
+++ www/pages/_document.js
@@ -2,21 +2,29 @@ import Document, { Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-  static getInitialProps({ renderPage }) {
-    // Step 1: Create an instance of ServerStyleSheet
-    // const sheet = new ServerStyleSheet();
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
 
-    // // Step 2: Retrieve styles from components in the page
-    // const page = renderPage((App) => (props) =>
-    //   sheet.collectStyles(<App {...props} />),
-    // );
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+        })
 
-    // // Step 3: Extract the styles as <style> tags
-    // const styleTags = sheet.getStyleElement();
-
-    // // Step 4: Pass styleTags as a prop
-    // return { ...page, styleTags };
-    return {}
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        // ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
   }
 
   render() {
@@ -26,9 +34,6 @@ export default class MyDocument extends Document {
             <link rel="shortcut icon" href="/static/favicon.png" />
             <meta name="theme-color" content="#e9c46a" />
             <link rel="manifest" href="/static/manifest.json" />
-            {
-              //this.props.styleTags
-            }
         </Head>
         <body>
             <Main />
